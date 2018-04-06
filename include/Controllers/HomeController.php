0b1d8d1687de9ea1bccea4f482689563fc243e77
Akhtem <akhtem.veliliaiev@mediatriple.com>
2018-04-06 08:39:08 +0300

    changes

--- include/Controllers/HomeController.php
+++ include/Controllers/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Controllers;
 
 class HomeController extends BaseController{
 
+    /**
+     * @return string
+     * @throws \App\Exception\BasicException
+     */
     public function dashboard(){
         return self::view('dashboard.php',[
             'user' => $this->auth->getUser()
